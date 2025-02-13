5714d9f2529ba02082a02cb5e1c2bed83e6176a7
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-02-13 20:28:29 +0100

    Add project link element selection to getUserInputs function

--- resume project/assets/js/app.js
+++ resume project/assets/js/app.js
@@ -77,8 +77,10 @@ const getUserInputs = () => {
   let eduDescriptionElem = document.querySelectorAll('.edu_description');
 
 
+// Projects 
 
   let projItemElem = document.querySelectorAll('.proj_title');
+  let projLinkElem = document.querySelectorAll('.proj_link');
 
   return{
     firstname: firstnameElem.value,
